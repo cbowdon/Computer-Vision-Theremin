8109959d1061e5cf1c72964a4c2952c171b81e00
Chris Bowdon <c.bowdon@gmail.com>
2013-02-23 18:40:21 +0000

    Implemented linear interpolation.

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,5 +1,7 @@
 #include <string>
+#include <ostream>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <tuple>
 #include <utility>
@@ -7,8 +9,14 @@
 #include <opencv2/core/core.hpp>
 #include "VideoProvider.hpp"
 
-typedef std::pair<size_t,cv::Point> datum;
+typedef std::pair<size_t,cv::Point2f> datum;
 typedef std::vector<datum> dataVec;
+
+std::ostream& operator<<(std::ostream& ostr, const datum d)
+{
+	ostr << std::get<0>(d) << ", " << std::get<1>(d);
+	return ostr;
+}
 
 static bool pause = false;
 static dataVec clickData;
@@ -19,29 +27,44 @@ static void mouseClick (int event, int x, int y, int, void*)
 {
 	if (event == CV_EVENT_LBUTTONDOWN)
 	{
-		cv::Point point(x, y);
+		cv::Point2f point(x, y);
 		clickData.push_back(datum(frameCount, point));
 		pause = false;
 	}
 }
 
 // Fills dataOut with linear interpolation of dataIn's points
-static void interpolate(const dataVec& dataIn, dataVec& dataOut)
+static void lerp(const dataVec& dataIn, dataVec& dataOut)
 {
 
-	dataOut.resize(dataIn.size() * 10);
+//	dataOut.resize(dataIn.size() * 10);
 
-	// TODO - implement interpolation
-	std::transform
-	(
-	 	begin(dataIn),
-		end(dataIn),
-		begin(dataOut),
-		[](const datum& d) 
-		{ 
-			return d; 
+	dataVec::const_iterator it = begin(dataIn), nd = end(dataIn);
+
+	float fc0 = 0, fc1 = 0;
+	cv::Point2f pt0, pt1;
+	for (it = begin(dataIn); it != nd; it++)
+	{
+		fc1 = std::get<0>(*it);
+		pt1 = std::get<1>(*it);
+
+		float nFrame = fc1 - fc0;
+
+		float xDist = pt1.x - pt0.x;
+		float yDist = pt1.y - pt0.y;
+
+		float xIncrement = xDist / nFrame;
+		float yIncrement = yDist / nFrame;
+
+		for (size_t i = 0; i < nFrame; i++)
+		{
+			cv::Point2f interPt(i * xIncrement + pt0.x, i * yIncrement + pt0.y);
+			dataOut.push_back(datum(fc0 + i, interPt));
 		}
-	);
+
+		std::swap(fc0, fc1);
+		std::swap(pt0, pt1);
+	}
 }
 
 int main (int argc, char** argv)
@@ -75,15 +98,26 @@ int main (int argc, char** argv)
 		cv::waitKey(10);
 	}
 
-	dataVec interpolatedData;
-	interpolate(clickData, interpolatedData);
+	dataVec lerpdData;
+	lerp(clickData, lerpdData);
 
 	std::for_each
 	(	
-	 	begin(interpolatedData),
-		end(interpolatedData),
-		[](datum& d) { std::cout << std::get<1>(d) << std::endl; }
+	 	begin(lerpdData),
+		end(lerpdData),
+		[&frame](datum& d) 
+		{ 
+			cv::circle(frame, std::get<1>(d), 2, cv::Scalar(255,0,0), 3, 8);
+//			std::cout << std::get<1>(d) << std::endl; 
+			std::cout << d << std::endl;
+		}
 	);
+	bool quit = false;
+	while (!quit)
+	{
+		cv::imshow(sampleFile, frame);
+		quit = cv::waitKey(5) == 'q';
+	}
 
 	return EXIT_SUCCESS;
 }
